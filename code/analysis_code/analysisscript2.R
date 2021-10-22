92c2bc2628ecdfdf555e4ec596159dd101b0afe6
Carter Coleman <cac64024@uga.edu>
2021-10-21 21:39:30 -0400

    MOD9_pt2 - CC

--- code/analysis_code/analysisscript2.R
+++ code/analysis_code/analysisscript2.R
@@ -9,6 +9,7 @@ output: html_document
 library(here)
 library(tidymodels)
 library(dplyr)
+library(tidyverse) #Added by Carter Coleman to run autoplot().
 ```
 
 
@@ -165,3 +166,175 @@ Nausea_runnynose_aug %>%
 Nausea_runnynose_aug %>% 
   roc_curve(truth = Nausea, .pred_No) %>% 
   autoplot()
+
+
+#########CARTER COLEMAN MODULE 9 ANALYSIS###########
+
+## Assessing fit model for Body Temperature and all other outcomes.
+
+  
+#### Creating a Recipe and Workflow
+  
+  
+# At this stage, our data is split into training and test data sets. 
+# Now, we need to create a recipe and workflow to help process the train data 
+# for model building. The output is a function that will run the entire linear 
+# regression model for any data set. Therefore, we can run the same model for 
+# the train and test data frames to create the exact same analysis workflow to 
+# ensure comparison.
+
+#Creates recipe
+Recipe_BT <- recipe(BodyTemp ~ ., data = train_data)
+
+#Define logistical regression model pipe
+linear_mod <- linear_reg() %>% 
+  set_engine("lm") %>%
+  set_mode("regression")
+
+#Create workflow that adds our recipe and model
+BT_lm_wflow <- 
+  workflow() %>% 
+  add_model(linear_mod) %>% 
+  add_recipe(Recipe_BT)
+  
+  #### Modeling Using the Workflow
+  
+# Using the workflow above, lets not fit the model to the train data set (Recipe_BT)._
+
+# Defining a command that runs model fitting to Recipe_BT
+BT_fit <- 
+  BT_lm_wflow %>% 
+  fit(data = train_data)
+
+#Pull linear regression fit model using parsnip()
+BT_fit %>% 
+  extract_fit_parsnip() %>% 
+  tidy()
+  
+### Using Workflows to Make Predictions
+  
+#Now that we have our modeling workflow, we are going to use the fitted model 
+#to predict values in the test data set.
+
+#predict values in test data
+predict(BT_fit, test_data)
+  
+#Additionally, we can use the augment() function to predict outcomes in the 
+#test data. But unlike the predict() function, augment() includes prediction residuals._
+
+#Predict outcomes using augment in test data
+BT_aug_test <- 
+  augment(BT_fit, test_data)
+  
+### Using RMSE to Assess Model Fit
+  
+#At this point, we have a fit model, a workflow for that model, and have some 
+#predictions made from a test subset of our data. 
+#Now, we need to use test predictions to assess if the model can predict 
+#values that reflect our actual data. In particular, we will be using
+#RMSE as our metric.
+
+#### Assessing fit for test data predictions
+
+##Test Data##
+
+#Calculate RMSE for test data
+BT_aug_test %>%
+  rmse(truth = BodyTemp, .pred)
+
+# RSME = 1.03
+
+##Train Data##
+
+#Predict outcomes using augment in training data
+BT_aug_train <- 
+  augment(BT_fit, train_data)
+
+#Calculate RSME for training data
+BT_aug_train %>%
+  rmse(truth = BodyTemp, .pred)
+
+#RSME = 1.15
+  
+##The resulting RSME values were 1.03 and 1.15 for the test data model and 
+#the training data model, respectively. This suggests that there is a 
+#low amount of variance between the model and both data sets. Therefore, 
+#the models are good fits to our data and can predict outcomes for Body Temperature.
+  
+#######Assessing fit model for Body Temperature and Runny Nose.########
+  
+#Creating a Recipe and Workflow
+  
+#Create a recipe and workflow to help process the train data for model building.
+
+#Creates recipe
+Recipe_BT2 <- recipe(BodyTemp ~ RunnyNose, data = train_data)
+
+#Define linear regression model pipe
+linear_mod <- linear_reg() %>% 
+  set_engine("lm") %>%
+  set_mode("regression")
+
+#Create workflow that adds our recipe and model
+BT2_wflow <- 
+  workflow() %>% 
+  add_model(linear_mod) %>% 
+  add_recipe(Recipe_BT2)
+  
+#### Modeling Using the Workflow
+  
+  
+#Use workflow to fit the model to the train data set (Recipe_BT2).
+
+#Defining a command that runs model fitting to Recipe_BT2.
+BT2_fit <- 
+  BT2_wflow %>% 
+  fit(data = train_data)
+
+#Pull linear reg fit model using parsnip()
+BT2_fit %>% 
+  extract_fit_parsnip() %>% 
+  tidy()
+  
+### Using Workflows to Make Predictions
+  
+  
+#Use the fitted model to predict values in the test data set.
+
+#predict values in test data
+predict(BT2_fit, test_data)
+
+  
+#Use the augment() function to predict outcomes in the test data.
+
+#Predict outcomes using augment in test data
+BT2_aug_test <- 
+  augment(BT2_fit, test_data)
+  
+### Using RMSE to Assess Model Fit
+  
+  
+#### Assessing fit for test data predictions
+  
+#Make RMSE for test data predictions.
+BT2_aug_test %>%
+  rmse(truth = BodyTemp, .pred)
+  
+#RMSE = 1.07
+
+####RMSE for Body Temperature as a function of Runny Nose in Train Data.
+
+#Predict outcomes using augment in training data.
+BT2_aug_train <- 
+  augment(BT2_fit, train_data)
+
+#Calculate RMSE for training data.
+BT2_aug_train %>%
+  rmse(truth = BodyTemp, .pred)
+
+#RMSE = 1.22
+  
+#The resulting RMSE values were 1.07 and 1.22 for the test data model and the 
+#training data model, respectively. This suggests that there is a 
+#low amount of variance between the model and both data sets. Therefore, 
+#the models are good fits to our data and can predict outcomes for Body Temperature.
